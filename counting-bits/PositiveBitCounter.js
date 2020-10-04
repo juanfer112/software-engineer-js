f03e409ad9e8914f41933417349066bbce1291d3
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-10-04 22:31:58 +0000

    Class created: PositiveBitCounter class was created and passed two test

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,17 @@
-function Count (input) {
-  throw new Error('Not implemented')
+class PositiveBitCounter {
+  static count (input) {
+    const result = [0]
+    if (input >= 0) {// Defensive programming practices.....
+      console.log(" Thi is a positive number")
+    } else {
+      throw new RangeError('You can not type a negative number')
+    }
+
+    return result
+  }
+
+
 }
 
-module.exports = { Count }
+module.exports = PositiveBitCounter
+
--- counting-bits/PositiveBitCounter.test.js
+++ counting-bits/PositiveBitCounter.test.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 describe('PositiveBitCounter', function () {
   it('Should return a RangeError when a negative value is supplied', function () {
     assert.throws(
-      () => PositiveBitCounter.Count(-2),
+      () => PositiveBitCounter.count(-2),
       function isRangeError (error) {
         return (error instanceof RangeError)
       })
@@ -12,19 +12,20 @@ describe('PositiveBitCounter', function () {
 
   it('Should return zero occurrences for input = 0', function () {
     let expected = [0]
-    let actual = PositiveBitCounter.Count(0)
+    let actual = PositiveBitCounter.count(0)
     assert.deepEqual(actual, expected)
   })
 
   it('Should return the expected count for input = 1', function () {
     let expected = [1, 0]
-    let actual = PositiveBitCounter.Count(1)
+    let actual = PositiveBitCounter.count(1)
     assert.deepEqual(actual, expected)
   })
 
   it('Should return the expected count for input = 161', function () {
     let expected = [3, 0, 5, 7]
-    let actual = PositiveBitCounter.Count(161)
+    let actual = PositiveBitCounter.count(161)
     assert.deepEqual(actual, expected)
   })
 })
+
