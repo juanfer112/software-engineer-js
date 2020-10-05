dee55388c9cd3f3525d724d3f314a742b8b238b1
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-10-05 08:05:20 +0000

    reverse function: created a function return return binary  reverse

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,13 +1,19 @@
 class PositiveBitCounter {
-  static count (input) {
-    const result = [0]
-    if (input >= 0) {// Defensive programming practices.....
-      console.log(" Thi is a positive number")
-    } else {
-      throw new RangeError('You can not type a negative number')
-    }
+    static count (input) {
+        const result = [0]
+        if (input >= 0) {// Defensive programming practices.....
+            const binary = input.toString(2)// convert number to binary
+            const reverseBinary = this.reverseString(binary)//apply a reverse to binary
+
+        } else {
+        throw new RangeError('You can not type a negative number')
+        }
 
     return result
+    }
+
+    static reverseString (str) {  
+        return str.split('').reverse().join('') //Rearrange: switch the string secuency from end to the beginning
   }
 
 
